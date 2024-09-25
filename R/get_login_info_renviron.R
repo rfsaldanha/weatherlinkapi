efb4926bd9a9e2b03fe3847d2d4b5771c31d877a
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-25 10:07:56 +0200

    Update keys names

--- R/get_login_info_renviron.R
+++ R/get_login_info_renviron.R
@@ -4,11 +4,11 @@ get_login_info_renviron <- function(){
     x_api_secret = Sys.getenv("weatherlink_x_api_secret")
   )
 
-  if(res$key == ""){
+  if(res$api_key == ""){
     cli::cli_abort("WeatherLink API key not provided and not found on renviron. Please provide it using the weatherlink_api_key and weatherlink_x_api_secret values on environ.")
   }
 
-  if(res$secret == ""){
+  if(res$x_api_secret == ""){
     cli::cli_abort("WeatherLink API secret not provided and not found on renviron. Please provide it using the weatherlink_api_key and weatherlink_x_api_secret values on environ.")
   }
 
