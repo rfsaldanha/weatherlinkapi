a5d92d97ee254fe3a362739e6b38add16ebc1ca8
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-25 12:06:11 +0200

    Add package dependencies

---
+++ R/parse_sensor.R
@@ -0,0 +1,17 @@
+# Adapted from https://github.com/basf/rweatherlink/blob/master/R/parsers.R
+
+#' @importFrom rlang .data
+parse_sensor <- function(sensor){
+  if (length(sensor$data) == 0){
+    cli::cli_abort("No data to parse.")
+  }
+    
+  suppressWarnings({
+    data <- data.table::setDF(data.table::rbindlist(sensor$data, fill = TRUE))
+  })
+
+  out <- tibble::tibble(lsid = sensor$lsid, sensor_type = sensor$sensor_type, data) |>
+    dplyr::mutate(ts = lubridate::as_datetime(.data$ts, tz = tz))
+  
+  return(out)
+}
